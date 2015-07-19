2a0ecb194751ffd46169bf6affba10d7ae259679
labeneko <takahiro.tsuno1@gmail.com>
2015-07-20 01:01:47 +0900

    おい！

--- wp-content/themes/plainly/header.php
+++ wp-content/themes/plainly/header.php
@@ -20,14 +20,11 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'plainly' ); ?></a>
 	<div id="content" class="site-content">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
 			<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</div>
 			</header><!-- #masthead -->
