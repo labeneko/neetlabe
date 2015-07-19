96c12637d8d21b593fe422d422b6ec84f16c89a3
labeneko <takahiro.tsuno1@gmail.com>
2015-07-20 00:23:56 +0900

    メニューなんていらん！

--- wp-content/themes/plainly/header.php
+++ wp-content/themes/plainly/header.php
@@ -30,8 +30,4 @@
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</div>
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle"><?php _e( 'Primary Menu', 'plainly' ); ?></button>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-			</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
