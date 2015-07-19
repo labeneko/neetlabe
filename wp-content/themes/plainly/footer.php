9109d4a12e990e8db11942f02f42cbf74ce78bde
labeneko <takahiro.tsuno1@gmail.com>
2015-07-19 23:26:32 +0900

    no message

--- wp-content/themes/plainly/footer.php
+++ wp-content/themes/plainly/footer.php
@@ -9,14 +9,6 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'plainly' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'plainly' ), 'WordPress' ); ?></a>
-			<span class="sep"> &middot; </span>
-			<?php printf( __( 'Theme: %1$s by %2$s', 'plainly' ), 'Plainly', '<a href="http://chris.ink/" rel="designer">Chris Rudzki</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
