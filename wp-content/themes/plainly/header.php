99547fd1912e1e4d2dd96efdb30d1642c968f10b
津野　貴大 <takahiro_tsuno@dwango.co.jp>
2018-04-02 11:39:42 +0900

    サイトのhttps化

--- wp-content/themes/plainly/header.php
+++ wp-content/themes/plainly/header.php
@@ -32,10 +32,10 @@
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
 			<header id="masthead" class="site-header" role="banner">
-			<div><img src="http://neetla.be/wp-content/uploads/2015/07/k-welcm.gif"></div>
-			<div><img src="http://neetla.be/wp-content/uploads/2015/07/coollogo_com-232372750.gif"></div>
+			<div><img src="https://neetla.be/wp-content/uploads/2015/07/k-welcm.gif"></div>
+			<div><img src="https://neetla.be/wp-content/uploads/2015/07/coollogo_com-232372750.gif"></div>
 			<div class="ticker">
                 <p>Welcome to にーとらべ！！！</p>
             </div>
-			<div><img src="http://neetla.be/wp-content/uploads/2015/07/labeneko.png" style="width: 700px;"></div>
+			<div><img src="https://neetla.be/wp-content/uploads/2015/07/labeneko.png" style="width: 700px;"></div>
 			</header><!-- #masthead -->
